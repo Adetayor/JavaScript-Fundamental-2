3ac57efb6ead0a88ab36fc42b155fb5a2703b794
Samuel <adetayo456@gmail.com>
2024-04-22 17:29:57 +0100

    developer skills continued

--- script2.js
+++ script2.js
@@ -44,3 +44,37 @@ console.log(amplitude);
 // We use google to find amplitude, We searched for "javascript get max value in an array"
 // Stack Overflow is a site of question and answer, most of the time someone already asked the questions, so go there to get the response
 // Usually the 1st or 2nd is the best solution but you scroll till you get the solution that you can implement we the tools that you already learnt
+
+// PROBLEM 2;
+// Functions should now receive 2 arrays of temps
+
+// 1) Understanding the problem
+// -- With 2 arrays, should we implement functionality twice?
+// NO! Just merge two arrays
+
+// 2) Breaking up into sub-problems
+// --Merge 2 arrays?
+
+const calcTempAmplitudeNew = function (t1, t2) {
+  //const array1 = ['a', 'b', 'c'];
+  //const array2 = ['d', 'e', 'f'];
+  //const array3 = array1.concat(array2);
+
+  const temps = t1.concat(t2);
+  console.log(temps);
+
+  let max = temps[0];
+  let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+     if (curTemp > max) max = curTemp;
+     if (curTemp < min) min = curTemp;
+   }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeNew = calcTempAmplitudeNew([3, 5,1], [9, 0, 5]);
+console.log(amplitudeNew);
